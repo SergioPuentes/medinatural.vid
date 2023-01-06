34414340b3fe43fcd79fabbc072c4baee66e3b36
Sergio Puentes <Se.puentesruiz@gmail.com>
2023-01-06 12:01:44 -0500

    construccion

---
+++ src/products/contacto.js
@@ -0,0 +1,19 @@
+const contacto = [
+    {
+        name:"Whatsapp",
+        description:"Escribenos por Whatsapp",
+        photo:""
+    },
+    {
+        name:"Estamos ubicados en San Gil-Santander",
+        description:"Cra 9 #9-35 Frente a la fiscalia municipal",
+        photo:""
+    },
+    {
+        name:"",
+        description:"",
+        photo:""
+    }
+
+
+]
